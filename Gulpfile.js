47f700b0fcee23567e895a5a6e5fdadedcf43a05
Ville Talonpoika <talonvi@gmail.com>
2016-02-01 22:02:32 +0200

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+
+gulp.task('sass', function () {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('coffee', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./app/src'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./src/*.coffee', ['coffee']);
+});
+
+gulp.task('default', function() {
+  gulp.start('sass', 'coffee');
+});
